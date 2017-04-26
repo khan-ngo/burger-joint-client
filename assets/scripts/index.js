563ba5a1b75f0fd2f65c61c31984257f2b1359e0
kqngo <khan.ngo@gmail.com>
2017-04-26 16:45:02 -0400

    Adding new files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,14 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+const bookEvents = require('./books/events')
+const authEvents = require('./auth/events.js')
+
+// on document ready
+$(() => {
+  authEvents.addHandlers()
+  bookEvents.addHandlers()
+
+
+})
