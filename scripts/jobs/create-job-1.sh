a79a99a85c09d0ca5363dd91018ff58468cefb65
kqngo <khan.ngo@gmail.com>
2017-05-13 10:28:35 -0400

    Updated with final touches

--- scripts/jobs/create-job-1.sh
+++ scripts/jobs/create-job-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN="BAhJIiVhMTIyOTBmNTkxNDc3NjM1OWM4ODA4NmNjMDM2M2M3ZAY6BkVG--5cc73b91c06b86205cf9bd609abe2d97ae466f03"
+TOKEN="BAhJIiU3NmU4MGE2MWM4ZjAzNmNhNTA5NTRlNTAyM2E2NzEzYgY6BkVG--55a06eedc2d28ae2fdcd5eb7db6f0f3098de8171"
 
 
   curl --include --request POST http://localhost:4741/jobs \
